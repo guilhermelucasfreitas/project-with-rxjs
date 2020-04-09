407a314da6f9f975c324fde4655aaf57d4a846e9
Guilherme Freitas Pereira <gfreitap@everis.com>
2020-04-09 19:59:05 -0300

    add caching observables in products and categories

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError, combineLatest, BehaviorSubject, Subject, merge } from 'rxjs';
-import { catchError, tap, map, scan } from 'rxjs/operators';
+import { catchError, tap, map, scan, shareReplay } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -38,8 +38,9 @@ export class ProductService {
         category: categories.find(c => product.categoryId === c.id).name,
         searchKey: [product.productName]
       }) as Product)
-    )
-  )
+    ),
+    shareReplay(1)
+  );
 
   private productSelectedSubject = new BehaviorSubject<number>(0);
   productSelectedAction$ = this.productSelectedSubject.asObservable();
@@ -52,7 +53,8 @@ export class ProductService {
         map(([products, selectedProductId]) => 
             products.find(product => product.id === selectedProductId)
           ),
-          tap(product => console.log('selectedProduct', product))
+          tap(product => console.log('selectedProduct', product)),
+          shareReplay(1)
       );
     
   //private for only service    
